10b6c3b30204d709233dc7475e732bf47113430a
Aryan Khorram <aryankhorram@Aryans-MBP.hsd1.wa.comcast.net>
2020-11-28 17:33:21 -0800

    first

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
